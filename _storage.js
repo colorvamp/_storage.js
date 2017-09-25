df99fab19ed2bbbf1d25bfe2d42e21366b27a728
sombra2eternity <sombra2eternity@gmail.com>
2017-09-25 23:07:49 +0200

    Update _storage.js

--- _storage.js
+++ _storage.js
@@ -1,20 +1,12 @@
-	var _storage = function(table){
-		this._db = false;
-		this.table = table;
-		this.version = 1;
-		this.isOpen = false;
+
+	var _storages = function(db){
+		this.db = db;
 	};
-	_storage.prototype.open = function(){
+	_storages.prototype.open = function(){
 		return new Promise(function (resolve, reject) {
 			if( this._db ){resolve({});}
 
-			this._open = indexedDB.open(this.table,this.version);
-			this._open.onupgradeneeded = function() {
-				this._db = this._open.result;
-				var store = this._db.createObjectStore(this.table);
-				var index = store.createIndex('path','path',{ unique: false });
-				var index = store.createIndex('file',['path','name'],{ unique: true });
-			}.bind(this);
+			this._open = indexedDB.open(this.db,this.version);
 			this._open.onsuccess = function(event) {
 				this._db = this._open.result;
 				resolve(event);
@@ -24,7 +16,73 @@
 			};
 		}.bind(this));
 	};
+	_storages.prototype.close = function(path){
+		this._db.close();
+		this._db = false;
+	};
+	_storages.prototype.names = function(){
+		return new Promise(function (resolve, reject) {
+			this.open()
+			.then(function(event){
+				resolve(this._db.objectStoreNames);
+				this.close();
+			}.bind(this),reject);
+		}.bind(this));
+	}
+
+	var _storage = function(db,table){
+		this._db = false;
+		this.db = db;
+		this.table = table || db;
+		this.version = 1;
+		this.isOpen = false;
+	};
+	_storage.prototype.open = function(debug){
+		return new Promise(function (resolve, reject) {
+			if( this._db ){resolve({});}
+
+			this._open = indexedDB.open(this.db,this.version);
+			this._open.onblocked = function(event) {
+				console.log('blocked');
+				return reject(event);
+			};
+			this._open.onupgradeneeded = function() {
+				this._db = this._open.result;
+				var store = this._db.createObjectStore(this.table);
+				var index = store.createIndex('path','path',{ unique: false });
+				var index = store.createIndex('file',['path','name'],{ unique: true });
+			}.bind(this);
+			this._open.onsuccess = function(event) {
+				this._db = this._open.result;
+				if( !this._db.objectStoreNames.contains(this.table) ){
+					this.version = parseInt(this._db.version) + 1;
+					if( this._db ){
+						this._db.close();
+						this._db = false;
+					}
+					return this.open().then(resolve, reject);
+				}
+				resolve(event);
+			}.bind(this);
+			this._open.onerror = function(event) {
+				if( (this.tmp = (event.target.error + '').match(/The requested version .*? is less than the existing version \(([0-9]+)\)./)) ){
+					this.version = this.tmp[1];
+					if( this._db ){
+						this._db.close();
+						this._db = false;
+					}
+					return this.open().then(resolve, reject);
+				}
+				reject(event);
+			}.bind(this);
+		}.bind(this));
+	};
+	_storage.prototype.close = function(path){
+		this._db.close();
+		this._db = false;
+	};
 	_storage.prototype.path = function(path){
+		if( !path ){return false;}
 		fileName = path.split('\\').pop().split('/').pop();
 		if( !fileName ){return false;}
 		filePath = path.substr(0,path.length - fileName.length);
@@ -72,15 +130,16 @@
 				request.onsuccess = function(event) {
 					if( request.result ){
 						objectStore.delete(request.result.primaryKey);
+					}else{
+						resolve(event);
 					}
-					resolve();
 				};
 			}.bind(this),function(event){
 				reject(event);
 			});
 		}.bind(this));
 	};
-	_storage.prototype.folder = function(path){
+	_storage.prototype.folder = function(path,cb){
 		return new Promise(function (resolve, reject) {
 			this.open()
 			.then(function(event){
@@ -98,25 +157,19 @@
 
 					var request = index.openCursor(path);
 					request.onsuccess = function(event) {
-						var ob = {
-							 'cursor': request.result
-							,'forEach': function(cb){
-								var k = 0;
-								if( request.result ) {
-									cb(request.result.value,k++);
-									request.result.continue();
-								}
-							}
-						};
-
-						resolve(ob);
+						if( request.result ) {
+							cb(request.result.value);
+							request.result.continue();
+						}else{
+							resolve(event);
+						}
 					};
 				};
 			}.bind(this),function(event){
 				reject(event);
 			});
 		}.bind(this));
-	};
+	}; 
 	_storage.prototype.store = function(path,content){
 		return new Promise(function (resolve, reject) {
 			var file = this.path(path);
